e2cf893c8cd8874283f7741bfecd74ce4a194bef
don-spiro <nddugwuoke@gmail.com>
2023-12-10 04:51:22 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    search == my_list[replace]
-    return [search]
+    my_list = [search]
+    new_list = my_list[replace]
+    return [new_list]
