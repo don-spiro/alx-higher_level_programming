a0c7bf9df452140a825afc6a501114a1449f1bf4
don-spiro <nddugwuoke@gmail.com>
2024-01-07 13:14:14 -0800

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for add_integer method"""
+
+
+def add_integer(a, b=98):
+
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    elif type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
+
+    if __name__ == "__main__":
+        import doctest
+        doctest.testfile("tests/0-add_integer.txt")
